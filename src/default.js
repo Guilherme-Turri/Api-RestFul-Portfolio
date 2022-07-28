a5f7dafbb7f4b18c26f415114041aed4ae9e1599
Guilherme Turri <guilhermeturri@icloud.com>
2022-07-28 16:01:24 -0300

    tst

--- src/default.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require('dotenv').config();
-const dbUser = process.env.DB_USER;
-const dbPass = process.env.DB_PASS;
-exports.default = {
-    dbUri: `mongodb+srv://${dbUser}:${dbPass}@cluster0.xmkj6.mongodb.net/?retryWrites=true&w=majority`,
-};
